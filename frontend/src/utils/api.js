1901f7a51c3d648f3fcd143ef0222df7cbc258f8
sanbaj-traid <sanbaj@triadsys.com>
2026-05-20 19:15:08 -0400

    Add runtime.txt for Render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // ---------------------------------------------------------
 const api = axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: 'https://confidant-latitude-clothes.ngrok-free.dev/',
+  baseURL: 'https://translator-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
