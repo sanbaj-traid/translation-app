c9b15a78b4a8e9265cf829cd0deed41b132faaa4
sanbaj-traid <sanbaj@triadsys.com>
2026-05-20 18:54:39 -0400

    Update ngrok URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // ---------------------------------------------------------
 const api = axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: 'https://abc123.ngrok-free.app',
+  baseURL: 'https://confidant-latitude-clothes.ngrok-free.dev/',
   headers: {
     'Content-Type': 'application/json',
   },
