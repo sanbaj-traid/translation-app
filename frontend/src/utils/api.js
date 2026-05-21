63c9c35520924b0085ec10bcd83afa1602bb139f
sanbaj-traid <sanbaj@triadsys.com>
2026-05-20 20:45:56 -0400

    Connect to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // ---------------------------------------------------------
 const api = axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: 'https://translator-backend.onrender.com',
+  baseURL: 'https://translation-app-d64t.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
